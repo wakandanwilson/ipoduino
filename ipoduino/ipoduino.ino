c4347625304e1bd27bd7943c75f1034f33f505af
wakandanwilson <166079536+wakandanwilson@users.noreply.github.com>
2024-05-17 16:50:19 -0700

    Update ipoduino.ino

--- ipoduino/ipoduino.ino
+++ ipoduino/ipoduino.ino
@@ -1,19 +1,28 @@
 #include <SoftwareSerial.h>
 #include <DFRobotDFPlayerMini.h>
-const int rxPin = 2;
+
+//initialize receiver and transceiver pins
+const int rxPin = 2; 
 const int txPin = 3;
 
+//creates player object
 DFRobotDFPlayerMini player;
 
 void setup(){
-  SoftwareSerial mySerial(rxPin, txPin);
+  //creates SoftwareSerial object called mySerial
+  SoftwareSerial mySerial(rxPin, txPin); 
+
+  //baud rate for laptop-arduino
   Serial.begin(9600);
+  //baud rate for RX-TX pins
   softwareSerial.begin(9600);
   
 }
 void loop(){
+
+  //checks if there's any data to read
   if (Serial.available() > 0){
-  byte incomingByte = Serial.read();
-  Serial.write(incomingByte);
+    byte incomingByte = Serial.read();
+    Serial.write(incomingByte);
   }
 }
